796e11761766006694f94155a4f4dbb4eb0e02c3
unknown <hetarth002@gmail.com>
2022-07-07 12:18:49 +0530

    time and post req intialized

--- app.js
+++ app.js
@@ -11,18 +11,25 @@ app.use(
     extended: true,
   })
 );
+const date = new Date();
+const currentYear = date.getFullYear();
 mongoose.connect("mongodb://localhost:27017/toyDB");
 
 const toySchema = new mongoose.Schema({
   name: String,
   desc: String,
-}); 
+});
 const Toy = new mongoose.model("Toy", toySchema);
 
-app.get("/", (req, res) => res.render("home"));
+app.get("/", (req, res) => res.render("home", { year: currentYear }));
 app.get("/login", (req, res) => res.render("login"));
 app.get("/register", (req, res) => res.render("register"));
 app.get("/directory", (req, res) => res.render("directory"));
 app.get("/add", (req, res) => res.render("addDir"));
 
+app.post("/register");
+
+app.post("/login");
+
+app.post("/add");
 app.listen(3000, console.log("Server running at port 3000"));

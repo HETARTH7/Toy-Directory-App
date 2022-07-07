2f534e30c028e4ed99e766fd9c2763d6076fb6f0
unknown <hetarth002@gmail.com>
2022-07-07 20:33:33 +0530

    delete op

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+var _ = require("lodash");
 const { default: mongoose } = require("mongoose");
 
 const app = express();
@@ -21,10 +22,24 @@ const toySchema = new mongoose.Schema({
 });
 const Toy = new mongoose.model("Toy", toySchema);
 
+app.post("/delete", (req, res) => {
+  const deleteId = req.body.delete;
+  Toy.findByIdAndRemove(deleteId, (err) => {
+    if (!err) {
+      console.log("Deleted");
+      res.redirect("/directory");
+    }
+  });
+});
+
 app.get("/", (req, res) => res.render("home", { year: currentYear }));
 app.get("/login", (req, res) => res.render("login"));
 app.get("/register", (req, res) => res.render("register"));
-app.get("/directory", (req, res) => res.render("directory"));
+app.get("/directory", (req, res) => {
+  Toy.find({}, (err, foundToy) => {
+    res.render("directory", { list: foundToy });
+  });
+});
 app.get("/add", (req, res) => res.render("addDir"));
 
 app.post("/register");
@@ -32,9 +47,15 @@ app.post("/register");
 app.post("/login");
 
 app.post("/add", (req, res) => {
-  const record = {
+  const record = new Toy({
     name: req.body.toyName,
     desc: req.body.toyDesc,
-  };
+  });
+  record.save();
+  res.redirect("/directory");
+});
+
+app.post("/directory", (req, res) => {
+  res.redirect("/add");
 });
 app.listen(3000, console.log("Server running at port 3000"));

e467ed3f003a08dfdeb99d85839fc29d9e73a719
unknown <hetarth002@gmail.com>
2022-07-07 09:20:19 +0530

    routes setup

--- app.js
+++ app.js
@@ -14,5 +14,7 @@ app.use(
 app.get("/", (req, res) => res.render("home"));
 app.get("/login", (req, res) => res.render("login"));
 app.get("/register", (req, res) => res.render("register"));
+app.get("/directory", (req, res) => res.render("directory"));
+app.get("/add", (req, res) => res.render("addDir"));
 
 app.listen(3000, console.log("Server running at port 3000"));
